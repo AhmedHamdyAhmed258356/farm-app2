7e025767eb322521824d76a9e77fb1e8d53a9624
Ahmed Hamdy <ah0934446@gmail.com>
2026-03-16 03:24:29 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,29 +1,29 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const User = sequelize.define('User', {
-  name: { type: DataTypes.STRING },
-  username: { type: DataTypes.STRING, allowNull: false, unique: true },
-  password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.ENUM('admin', 'worker', 'accountant'), defaultValue: 'worker' }
+    name: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.ENUM('admin', 'worker', 'accountant'), defaultValue: 'worker' }
 }, {
-  hooks: {
-    beforeCreate: async (user) => {
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(user.password, salt);
-    },
-    beforeUpdate: async (user) => {
-      if (user.changed('password')) {
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-      }
+    hooks: {
+          beforeCreate: async (user) => {
+                  const salt = await bcrypt.genSalt(10);
+                  user.password = await bcrypt.hash(user.password, salt);
+          },
+          beforeUpdate: async (user) => {
+                  if (user.changed('password')) {
+                            const salt = await bcrypt.genSalt(10);
+                            user.password = await bcrypt.hash(user.password, salt);
+                  }
+          }
     }
-  }
 });
 
 User.prototype.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+    return await bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = User;
