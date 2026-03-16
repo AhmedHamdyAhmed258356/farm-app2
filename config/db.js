fbd4057016c9178bba2bd64dede78edb11d21a75
Ahmed Hamdy <ah0934446@gmail.com>
2026-03-16 03:26:52 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,29 +4,29 @@ const isProd = process.env.DATABASE_URL;
 
 const sequelize = isProd
   ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      },
-      logging: false
-    })
-  : new Sequelize({
-      dialect: 'sqlite',
-      storage: './database.sqlite',
-      logging: false
+          dialect: 'postgres',
+          protocol: 'postgres',
+          dialectOptions: {
+                    ssl: {
+                                require: true,
+                                rejectUnauthorized: false
+                    }
+          },
+          logging: false
+  })
+    : new Sequelize({
+            dialect: 'sqlite',
+            storage: './database.sqlite',
+            logging: false
     });
 
 const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('SQLite Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+    try {
+          await sequelize.authenticate();
+          console.log('Database connection has been established successfully.');
+    } catch (error) {
+          console.error('Unable to connect to the database:', error);
+    }
 };
 
 module.exports = { sequelize, connectDB };
